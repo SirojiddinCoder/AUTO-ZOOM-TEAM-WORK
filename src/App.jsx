9d155bdc3b6b76398da7bd5208e23886a61fcf2d
Xurshida65 <xurshidasamigova65@gmail.com>
2024-06-25 17:16:06 +0500

    ytty

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,20 @@ import AboutUs from './Pages/AboutUs/AboutUs';
 import Contact from './Pages/Contact/Contact';
 import Blog from './Pages/Blog/Blog';
 import './App.css'
+import SinglePage from './Pages/SinglePage/SinglePage';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars/>} />
+        <Route path="/cars" element={<Cars />} />
         <Route path="/brand" element={<Brand />} />
-        <Route path="/services" element={<Services />} /> 
+        <Route path="/services" element={<Services />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/carsdetails:id" element={<SinglePage />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </div>
--- src/Pages/SinglePage/SinglePage.jsx
+++ src/Pages/SinglePage/SinglePage.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function SinglePage() {
+    const {id} = useParams()
+    const carDetail = `https://autoapi.dezinfeksiyatashkent.uz/api/cars/${id}`
+    const [aboutCar, getAboutCar]  = useState()
+    console.log(id);
+   const getDetails = fetch(carDetail).then(res=>res.json()).then((data)=>console.log(data?.car_id))
   return (
-    <div>
-      
-    </div>
+   <></>
   )
 }
 
