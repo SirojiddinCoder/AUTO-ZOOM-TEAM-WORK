7f409894660f66af41d3bb6a7d7747c37fca5b8c
SirojiddinCoder <sirojiddincoder@gmail.com>
2024-06-27 04:34:04 -0700

    navbar fixed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,74 +1,59 @@
-
 import { useRef } from "react";
 import { FaBars, FaSearch, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import lang from '../../assets/RU1.jpg'
+import RU from '../../assets/RU1.jpg'
+import EN from '../../assets/eng.svg'
 import logo from '../../assets/LOGO.svg'
 import './navbar.css'
-
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa';  // Importing the search icon from react-icons
-import './navbar.css';
-import logo from '../../assets/LOGO.svg';
-import US from '../../assets/US.svg';
-import RU from '../../assets/RU.svg';
-
-import Main from '../../Components/Main/Main';
-import i18n from '../../i18n';
-
-
-
 function Navbar() {
-	const navRef = useRef();
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
-
-	return (
-		<header>
-			<div className="container">
-      <div className="flags">
-				<img className="flag" src={lang} alt="US Flag" />
-				<img className="flag" src={lang} alt="RU Flag" />
-			</div>
-			<div className="search">
-				<FaSearch className="search-icon" />
-				<input type="text" placeholder="Search..." />
-			</div>
-			<div className="logo">
-				<img src={logo} alt="Logo" />
-			</div>
-			<nav ref={navRef}>
-				<Link className="nav-item" to="/">Home</Link>
-				<Link className="nav-item" to="/cars">Cars</Link>
-				<Link className="nav-item" to="/brand">Brand</Link>
-				<Link className="nav-item" to="/services">Services</Link>
-				<Link className="nav-item" to="/about">About</Link>
-				<Link className="nav-item" to="/contact">Contact</Link>
-				<Link className="nav-item" to="/blog">Blog</Link>
-     
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-      </div>
-      <div className="home">
+  return (
+    <>
+      <header>
         <div className="container">
-          Lorem ipsum dolor sit amet.
+          <div className="flags">
+            <img className="flag1" src={RU} alt="US Flag" />
+            <img className="flag1" src={EN} alt="RU Flag" />
+          </div>
+          <div className="search">
+            <FaSearch className="search-icon" />
+            <input type="text" placeholder="Search..." />
+          </div>
+          <div className="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+          <nav ref={navRef}>
+            <div className="nav-container">
+              <div className="nav-items">
+                <Link className="nav-item" to="/" onClick={() => setLoader(false)}>Home</Link>
+                <Link className="nav-item" to="/cars" onClick={() => setLoader(false)}>Cars</Link>
+                <Link className="nav-item" to="/brand" onClick={() => setLoader(false)}>Brand</Link>
+                <Link className="nav-item" to="/services" onClick={() => setLoader(false)}>Services</Link>
+                <Link className="nav-item" to="/about" onClick={() => setLoader(false)}>About</Link>
+                <Link className="nav-item" to="/contact" onClick={() => setLoader(false)}>Contact</Link>
+                <Link className="nav-item" to="/blog" onClick={() => setLoader(false)}>Blog</Link>
+              </div>
+              <a className="nav-tel" href="tel: +971 (55) 846 21 24">+971 (55) 846 21 24</a>
+            </div>
+            <button
+              className="nav-btn nav-close-btn"
+              onClick={showNavbar}>
+              <FaTimes />
+            </button>
+          </nav>
+          <button
+            className="nav-btn"
+            onClick={showNavbar}>
+            <FaBars />
+          </button>
         </div>
-      </div>
-      
-		</header>
-	);
+      </header>
+    </>
+  )
 }
 
 export default Navbar;
