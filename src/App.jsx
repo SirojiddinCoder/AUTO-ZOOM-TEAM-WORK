fb27b89c8b5a15b8f3db056c0c6710ac4a796fdb
Your Name <you@example.com>
2024-06-27 17:05:30 +0500

    link color changed2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Cars from './Pages/Cars/Cars';
 import Brand from './Pages/Brand/Brand';
@@ -7,40 +7,30 @@ import Services from './Pages/Services/Services';
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Contact from './Pages/Contact/Contact';
 import Blog from './Pages/Blog/Blog';
-import SinglePage from './Pages/SinglePage/SinglePage';
+import './App.css'
 import Loader from './Components/Loader/Loader';
-import './App.css';
-// import CarsInfo from './Pages/CarsInfo/CarsInfo';
 import Navbar from './Components/Navbar/Navbar';
-
 import Footer from './Components/Footer/Footer';
 
-// import Navbar from './Components/Navbar/navbar';
-
-
 function App() {
-  const [loader, setLoader] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoader(true);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  const [loader, setLoader] = useState(false)
+  setTimeout(() => {
+    setLoader(true)
+  }, 2000);
   return (
     <div>
-      <Navbar />
+      {loader ? "" : 
+      <Loader/>
+      }
+      <Navbar setLoader={setLoader}/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars" element={<Cars/>} />
         <Route path="/brand" element={<Brand />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} /> 
+        <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/carsdetails/:id" element={<SinglePage />} />
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer/>
