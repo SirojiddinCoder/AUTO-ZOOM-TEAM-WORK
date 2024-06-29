57ad6349fd8800038428ff64a463c4a76bd0fa2b
Xurshida65 <xurshidasamigova65@gmail.com>
2024-06-29 13:15:16 +0500

    updated

--- src/Pages/SinglePage/mock/carsData.js
+++ src/Pages/SinglePage/mock/carsData.js
@@ -6,14 +6,17 @@ const carsData = [
   {
     component: BsWhatsapp,
     style: "#5AEC5A",
+    getInTouch: "https://wa.me/971558462124",
   },
   {
     component: BiLogoTelegram,
     style: "#2374F8",
+    getInTouch: "https://t.me/+971558462124",
   },
   {
     component: LuPhone,
     style: "#FFB630",
+    getInTouch: "tel:+971558462124",
   },
 ];
 
@@ -21,22 +24,26 @@ const formInputs = [
   {
     type: "text",
     placeholder: "Name",
-    classname: "inputName"
+    classname: "inputName",
+    required: true,
   },
   {
     type: "text",
+    classname: "inputName",
     placeholder: "Phone",
-    classname: "inputName"
+    required: true,
   },
   {
     type: "text",
     placeholder: "Period",
-    classname: "inputName"
+    classname: "inputName",
+    required: false,
   },
   {
     type: "text",
     placeholder: "Details",
-    classname: "inputName"
+    classname: "inputName",
+    required: false,
   },
 ];
 export { carsData, formInputs} ;
