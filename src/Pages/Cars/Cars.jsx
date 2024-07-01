7c40cf6e08a9f91031bfcb934bc756a617781696
Your Name <you@example.com>
2024-07-01 14:48:54 +0500

    feds

--- src/Pages/Cars/Cars.jsx
+++ src/Pages/Cars/Cars.jsx
@@ -6,15 +6,6 @@ const Cars = ({cars, setCars}) => {
   return (
     <div>
       <CarsFilter cars={cars} setCars={setCars}/>
-
-import CarsFilter from "../../Components/CarsFilter/CarsFilter";
-
-const Cars = () => {
-
-  return (
-    <div>
-      <CarsFilter />
-
     </div>
   );
 };
