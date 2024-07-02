01856a30d78371a49bee8894a063fbf256f3ba05
Xurshida65 <xurshidasamigova65@gmail.com>
2024-07-02 11:30:01 +0500

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes, useParams } from 'react-router-dom';
+
+import { Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Cars from './Pages/Cars/Cars';
 import Brand from './Pages/Brand/Brand';
